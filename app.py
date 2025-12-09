38c7b2d747e7c354476ef8a1f291f2cb62f1dd1b
egomesPUC <39346896+egomesPUC@users.noreply.github.com>
2025-12-08 22:47:04 -0300

    teste

--- app.py
+++ app.py
@@ -6,3 +6,4 @@ st.set_page_config(
     page_title="Dashboard - Peças de Pessoas em Situação de Rua - TEMPORARIAMENTE FORA DO AR!!",
     layout="wide"
 )
+st.title("Dashboard - Peças de Pessoas em Situação de Rua - TEMPORARIAMENTE FORA DO AR!!")
